63b1f396c3ebbdff8b707e6a75cf3c3da674a3ba
林俊廷 <ms0491826@gmail.com>
2024-06-13 17:13:38 +0800

    'apiChanged'

--- components/config/api-path.tsx
+++ components/config/api-path.tsx
@@ -1,4 +1,5 @@
-export const API_SERVER = "http://localhost:3001";
+// export const API_SERVER = "http://localhost:3001";
+export const API_SERVER = "https://cart-server.fly.dev";
 
 export const JWT_LOGIN_POST = `${API_SERVER}/login-jwt`;
 export const JWT_CART_POST = `${API_SERVER}/api/cart`;
